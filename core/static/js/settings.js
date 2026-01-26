bbefec8a189b42592a5ed4f1580f3855489444e9
Farid Shykhaliev <jatix.com@mail.ru>
2026-01-26 16:47:53 +0300

    WebUI: - Mobile icon fix

--- core/static/js/settings.js
+++ core/static/js/settings.js
@@ -1386,7 +1386,7 @@ function renderSessionItem(s) {
 
     const date = new Date(s.created * 1000).toLocaleString();
     let iconPath = "M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z";
-    if (iconType === "mobile") iconPath = "M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2-2H8a2 2 0 00-2-2v14a2 2 0 002 2z";
+    if (iconType === "mobile") iconPath = "M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z";
     else if (iconType === "terminal") iconPath = "M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z";
 
     let userBadge = "";
